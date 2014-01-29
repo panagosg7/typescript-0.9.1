88ba4e240ad8f8b7edefbc0ced4f9d5cc65798d8
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-01-29 13:19:35 -0800

    Fix in nanogoto.sh script.

--- nanogoto.sh
+++ nanogoto.sh
@@ -7,7 +7,7 @@ FILENAME=$(basename $1)
 HSENV_PATH=/home/pvekris/Documents/research/liquid-js/.hsenv/bin
 NANO_PATH=/home/pvekris/Documents/research/liquid-js/.hsenv/cabal/bin/nanojs
 
-scp $1 $USER@$SERVER:$TMP_PATH/FILENAME
+scp $1 $USER@$SERVER:$TMP_PATH/$FILENAME
 
 ssh $USER@$SERVER "	export PATH=\$PATH:/home/pvekris/.root/bin ; \
 			source $HSENV_PATH/activate  > /dev/null ; \
